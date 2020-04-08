7f2f290ea5af6dfbd0245863efce3fae31b506ad
Nathan Perry <np@nathanperry.dev>
2020-04-08 06:52:17 -0400

    orDefault

--- lib/default.nix
+++ lib/default.nix
@@ -3,4 +3,6 @@ with lib;
 
 {
   tryCallPackage = path: overrides: if pathExists path then callPackage path overrides else null;
+
+  orDefault = arg: default: if arg != null then arg else default;
 }
