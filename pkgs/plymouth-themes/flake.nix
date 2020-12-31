e6324ea1426de08b7780baba5a6625643e1f88cb
Nathan Perry <np@nathanperry.dev>
2020-12-30 21:50:09 -0500

    plymouth themes

---
+++ pkgs/plymouth-themes/flake.nix
@@ -0,0 +1,55 @@
+# Note: this flake derives the entries in its outputs.<system>.packages
+# from walking the directories in inputs.plymouth-themes and heuristically
+# checking whether each dir looks like a theme (as well as explicitly
+# blacklisting the /templates/ directory). I wrote it this way because
+# it makes things easier if the author decides to modify the repo.
+#
+# Implementing things this way requires an unsafeDiscardStringContext in
+# ./package.nix. This is safe because this is a flake, and hence
+# reproducible at a given flake.lock. If, however, the underlying repo
+# fundamentally changes its structure, updating the input may lead to
+# surprising behavior (e.g. all theme packages vanishing, if ./package.nix
+# can't find directories that it thinks are themes anymore).
+
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    plymouth-themes = {
+      url = "github:adi1090x/plymouth-themes";
+      flake = false;
+    };
+  };
+
+  description = "plymouth themes";
+
+  outputs = { self, nixpkgs, flake-utils, plymouth-themes }: (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      stdenv = pkgs.stdenv;
+
+      themes = builtins.readDir plymouth-themes;
+
+      mkPackage = { name, path, ... }:
+        stdenv.mkDerivation {
+          pname = "plymouth-themes-${name}";
+          version = plymouth-themes.rev;
+
+          src = plymouth-themes;
+
+          buildPhase = ''
+            echo ${name} ${path} ${builtins.toJSON themes}
+            ls -al
+            exit 1
+          '';
+        };
+
+
+      pkg = pkgs.callPackage ./package.nix { inherit plymouth-themes; };
+
+    in {
+      packages = pkg;
+    })
+  );
+}
---
+++ pkgs/plymouth-themes/package.nix
@@ -0,0 +1,92 @@
+{
+  stdenv,
+  lib,
+  plymouth-themes,
+  breakpointHook,
+  ...
+}:
+
+let
+  themes = builtins.readDir plymouth-themes;
+
+  mkPackage = { name, path, ... }:
+    stdenv.mkDerivation {
+      pname = "plymouth-themes-${name}";
+      version = plymouth-themes.rev;
+
+      src = plymouth-themes;
+
+      installPhase = ''
+        cd ${path}
+
+        if [ ! -e "${name}.script" ]; then
+          echo "${name} lacking script file" >&2
+          exit 1
+        fi
+
+        if grep -qE '/usr|/share|/lib' "${name}.script"; then
+          echo "${name} script file appears to refer to fhs env" >&2
+          exit 1
+        fi
+
+        if [ ! -e "${name}.plymouth" ]; then
+          echo "${name} lacks plymouth file" >&2
+          exit 1
+        fi
+
+        echo "${name} ok" >&2
+
+        mkdir -p  "$out/share/plymouth/themes"
+        cp -R .   "$out/share/plymouth/themes/${name}"
+        cd        "$out/share/plymouth/themes/${name}"
+
+        chmod -R u+w .
+
+        rm -f ${name}.plymouth
+        rm -f LICENSE
+
+        echo generating plymouth file: >&2
+
+        tee "${name}.plymouth" >&2 <<EOF
+        [Plymouth Theme]
+        Name=${name}
+        ModuleName=script
+
+        [script]
+        ImageDir=$out/share/plymouth/themes/${name}
+        ScriptFile=$out/share/plymouth/themes/${name}.script
+        EOF
+      '';
+    };
+
+  isThemeDir = dir: with builtins;
+  let
+    name = baseNameOf dir;
+    files = readDir dir;
+    regularFiles = filter (filename: files.${filename} == "regular") (attrNames files);
+
+    hasPlymouth = lib.any (filename: filename == "${name}.plymouth")  regularFiles;
+    hasScript   = lib.any (filename: filename == "${name}.script")    regularFiles;
+  in name != "templates" && hasPlymouth && hasScript;
+
+  findThemes = rootDir: with builtins;
+  let
+    files     = readDir rootDir;
+    dirNames  = filter (filename: files.${filename} == "directory") (attrNames files);
+    dirs      = map (x: unsafeDiscardStringContext "${rootDir}/${x}") dirNames;
+
+    childThemes =
+      foldl'
+      (acc: x:
+        let
+          relPath = lib.removePrefix plymouth-themes x;
+          newThemeDirs = if isThemeDir x then { ${baseNameOf x} = relPath; } else findThemes x;
+        in acc // newThemeDirs)
+
+      {}
+      dirs;
+  in if isThemeDir rootDir then [rootDir] else childThemes;
+
+  themeDirs = findThemes plymouth-themes;
+
+in lib.mapAttrs (name: path: mkPackage { inherit name path; }) themeDirs
