04e19374698094765d1bf17bfc2f1d5d6329a6bf
Nathan Perry <np@nathanperry.dev>
2021-01-02 03:31:05 -0500

    overlays: remove dependency on nixpkgs

--- overlays/flake.nix
+++ overlays/flake.nix
@@ -2,17 +2,24 @@
   description = "standard overlays";
 
   outputs = { self, ... }: with builtins;
-  let
-    composeOverlays = foldl' pkgs.lib.composeExtensions (self: super: {});
-  in {
-    overlays = {
-      gnupg = (self: super: {
-        gnupg = super.gnupg.overrideAttrs (oldattrs: {
-          patches = oldattrs.patches ++ [ ./patches/scdaemon-shared-access.patch ];
-        });
-      });
-    };
+    let
+      composeExtensions = f: g: self: super:
+        let fApplied = f self super;
+            super' = super // fApplied;
 
-    overlay = composeOverlays builtins.attrValues self.overlays;
-  };
+        in fApplied // g self super';
+
+      composeOverlays = foldl' composeExtensions (self: super: {});
+
+    in {
+      overlays = {
+        gnupg = (self: super: {
+          gnupg = super.gnupg.overrideAttrs (oldattrs: {
+            patches = oldattrs.patches ++ [ ./patches/scdaemon-shared-access.patch ];
+          });
+        });
+      };
+
+      overlay = composeOverlays (attrValues self.overlays);
+    };
 }
